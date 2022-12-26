aef6e9425e157f838ccdda42b809a7ee99d748a4
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-26 23:55:13 +0200

    add sass

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,3 +1,5 @@
+import "./Content.scss";
+
 type ContentProps = {
   p1: string;
   p2: string;
@@ -6,7 +8,7 @@ type ContentProps = {
 const Content = ({ p1, p2 }: ContentProps) => {
   return (
     <>
-      <p>{p1}</p>
+      <p className="tomato">{p1}</p>
       <p>{p2}</p>
     </>
   );
