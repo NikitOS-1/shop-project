52a69172022afebab03b053d587e9b93362bdf55
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-27 00:11:28 +0200

    deleted test files

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Content from "./Content";
-
-type TitleProps = {
-  title: string;
-  text?: string;
-  id?: number | string;
-};
-
-const Title = ({ title, text = "TEST", id }: TitleProps) => {
-  return (
-    <>
-      <h1>
-        {text}
-        {title}
-        {id}
-      </h1>
-    </>
-  );
-};
 
 const App = () => {
   return (
     <>
-      <Title text="Hello" title=" React JS " id={`  ID : ${10}`} />
-      <Title title=" React JS " id={`  ID : ${10}`} />
-      <Content p1="p1" p2="p2" />
+      <p>test</p>
     </>
   );
 };
