fa5188297cfd10d6f06727f09f229cfc8576707b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-28 00:01:26 +0200

    add container MUI and style in components

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -5,27 +5,35 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import { green } from "@mui/material/colors";
 
 type Props = {};
 
 const Header = () => {
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+      <AppBar
+        position="static"
+        style={{
+          backgroundColor: "green",
+        }}>
+        <Container maxWidth="lg">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </Container>
       </AppBar>
     </>
   );
