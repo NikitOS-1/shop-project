4aa0dcccdf5de5226098b7a805546188c1b65224
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-27 23:46:32 +0200

    add icons material and component AppBar

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,37 +1,32 @@
 import style from "./Header.module.scss";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 type Props = {};
 
 const Header = () => {
   return (
     <>
-      <header className={style.header}>
-        <div className={style.logo}>
-          <h2>Еда как искусство</h2>
-        </div>
-        <nav className={style.nav}>
-          <ul>
-            <li>
-              <a href="#">Главная</a>
-            </li>
-            <li>
-              <a href="#">Портфолио</a>
-            </li>
-            <li>
-              <a href="#">Обо мне</a>
-            </li>
-            <li>
-              <a href="#">Книги</a>
-            </li>
-            <li>
-              <a href="#">Блог</a>
-            </li>
-            <li>
-              <a href="#">Связаться</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </>
   );
 };
