017f1e0e463fd845789cadc971626c1f27426d5c
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-27 23:38:20 +0200

    add normalize css (CssBaseLine)

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,9 +1,15 @@
 import Header from "../Header/Header";
+import CssBaseline from "@mui/material/CssBaseline";
 
 type Props = {};
 
 const App = (props: Props) => {
-  return <>{<Header />}</>;
+  return (
+    <>
+      <Header />
+      <CssBaseline />
+    </>
+  );
 };
 
 export default App;
