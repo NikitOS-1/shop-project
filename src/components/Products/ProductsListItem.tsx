561001426c016506cacd285a13f8512e75a5bd78
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-01-10 23:06:33 +0200

    tese

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -10,6 +10,7 @@ const ProductsListItem = (props: Props) => {
         <p className="product-desc">This is iPhone X</p>
         <div className="product-features">Type: Phone</div>
         <div className="product-features">Capacity: 64gb</div>
+        <div className="product-features">Capacity: 64gb</div>
         <div className="product-price">900$</div>
         <div className="product-price">900$</div>
       </CardContent>
