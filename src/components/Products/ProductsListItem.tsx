01ea60479120479b33e5b8a0413f3e72d53c8d9f
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-01-12 20:16:20 +0200

    rewrites in class components

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -5,6 +5,7 @@ import {
   CardContent,
   TextField,
 } from "@mui/material";
+import { Component, ReactNode } from "react";
 import "./ProductsListItem.scss";
 
 type Props = {
@@ -15,40 +16,35 @@ type Props = {
   price: number;
   image: string;
 };
-
-const ProductsListItem = ({
-  title,
-  desc,
-  type,
-  capasity,
-  price,
-  image,
-}: Props) => {
-  return (
-    <Card className="product-list-item">
-      <CardContent>
-        <div className="product-img">
-          <img src={image} alt="#" />
+class ProductsListItem extends Component<Props> {
+  render() {
+    const { title, desc, type, capasity, price, image } = this.props;
+    return (
+      <Card className="product-list-item">
+        <CardContent>
+          <div className="product-img">
+            <img src={image} alt="#" />
+          </div>
+          <h4 className="product-title">{title}</h4>
+          <p className="product-desc">{desc}</p>
+          <div className="product-features">
+            <span>Type:</span> {type}
+          </div>
+          <div className="product-features">
+            <span>Capacity:</span> {capasity}
+          </div>
+          <div className="product-price">{price}$</div>
+        </CardContent>
+        <div className="product-quantity">
+          <Button variant="outlined">-</Button>
+          <TextField size="small" value={1} />
+          <Button variant="outlined">+</Button>
         </div>
-        <h4 className="product-title">{title}</h4>
-        <p className="product-desc">{desc}</p>
-        <div className="product-features">
-          <span>Type:</span> {type}
-        </div>
-        <div className="product-features">
-          <span>Capacity:</span> {capasity}
-        </div>
-        <div className="product-price">{price}$</div>
-      </CardContent>
-      <div className="product-quantity">
-        <Button variant="outlined">-</Button>
-        <TextField size="small" value={1} />
-        <Button variant="outlined">+</Button>
-      </div>
-      <CardActions className="btn-wrap">
-        <Button variant="outlined">Add to cart</Button>
-      </CardActions>
-    </Card>
-  );
-};
+        <CardActions className="btn-wrap">
+          <Button variant="outlined">Add to cart</Button>
+        </CardActions>
+      </Card>
+    );
+  }
+}
 export default ProductsListItem;
