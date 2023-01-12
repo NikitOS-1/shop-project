ecd05b6073ac593275332fb4adc3832b6abde1cb
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-01-12 22:53:26 +0200

    REWRITE on function component

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -17,75 +17,44 @@ type Props = {
   image: string;
 };
 
-type State = {
-  count: number;
-  color: string;
-};
-
-class ProductsListItem extends Component<Props, State> {
-  state = {
-    count: 1,
-    color: "red",
-  };
-
-  onDecrementClick = () => {
-    this.setState((prevState: State) => ({
-      count: prevState.count - 1,
-    }));
-  };
-
-  onIncrementClick() {
-    this.setState((prevState: State) => ({
-      count: prevState.count + 1,
-    }));
-  }
-
-  changeColor = () => {
-    this.setState((prevState: State) => ({
-      color: prevState.color === "green" ? "red" : "green",
-    }));
-  };
-
-  render() {
-    const { title, desc, type, capasity, price, image } = this.props;
-
-    return (
-      <Card className="product-list-item">
-        <CardContent>
-          <div className="product-img">
-            <img src={image} alt="#" />
-          </div>
-          <h4 className="product-title">{title}</h4>
-          <p className="product-desc">{desc}</p>
-          <div className="product-features">
-            <span>Type:</span> {type}
-          </div>
-          <div className="product-features">
-            <span>Capacity:</span> {capasity}
-          </div>
-          <div className="color">
-            <p>Color:</p>
-            <div className={this.state.color} />
-            {/* <p>{this.state.color}</p> */}
-          </div>
-          <button onClick={() => this.changeColor()}>Change color</button>
-          <div className="product-price">{price}$</div>
-        </CardContent>
-        <div className="product-quantity">
-          <Button variant="outlined" onClick={this.onDecrementClick}>
-            -
-          </Button>
-          <TextField size="small" value={this.state.count} />
-          <Button variant="outlined" onClick={() => this.onIncrementClick()}>
-            +
-          </Button>
+const ProductsListItem = ({
+  title,
+  desc,
+  type,
+  capasity,
+  price,
+  image,
+}: Props) => {
+  return (
+    <Card className="product-list-item">
+      <CardContent>
+        <div className="product-img">
+          <img src={image} alt="#" />
         </div>
-        <CardActions className="btn-wrap">
-          <Button variant="outlined">Add to cart</Button>
-        </CardActions>
-      </Card>
-    );
-  }
-}
+        <h4 className="product-title">{title}</h4>
+        <p className="product-desc">{desc}</p>
+        <div className="product-features">
+          <span>Type:</span> {type}
+        </div>
+        <div className="product-features">
+          <span>Capacity:</span> {capasity}
+        </div>
+        <div className="color">
+          <p>Color: red</p>
+        </div>
+        <button>Change color</button>
+        <div className="product-price">{price}$</div>
+      </CardContent>
+      <div className="product-quantity">
+        <Button variant="outlined">-</Button>
+        <TextField size="small" value={1} />
+        <Button variant="outlined">+</Button>
+      </div>
+      <CardActions className="btn-wrap">
+        <Button variant="outlined">Add to cart</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default ProductsListItem;
