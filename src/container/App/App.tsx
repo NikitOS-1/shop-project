d38cd7d4e714c25931e1d89edb7ffb5c2ae95ff0
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-01-13 13:02:42 +0200

    add function addProductCard

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -13,14 +13,22 @@ type CardDataProps = {
 
 const App = (props: Props) => {
   const [cartData, setCartData] = useState<CardDataProps>({
-    totalCount: 10,
-    totalPrice: 100,
+    totalCount: 0,
+    totalPrice: 0,
   });
+
+  const addProductCard = (total: number, price: number) => {
+    setCartData((prevState: CardDataProps) => ({
+      totalCount: prevState.totalCount + total,
+      totalPrice: prevState.totalPrice + total * price,
+    }));
+  };
 
   return (
     <StyledEngineProvider injectFirst>
       <CssBaseline />
       <Header cartData={cartData} />
+      <button onClick={() => addProductCard(5, 500)}>add product</button>
       <Main />
     </StyledEngineProvider>
   );
