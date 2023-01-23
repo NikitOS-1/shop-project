f7e80501a6d90957aba2bec2801981e7f2708c3c
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-01-23 18:56:25 +0200

    ref

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,5 +1,62 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
 type Props = {};
+
+type Review = {
+  name: string;
+  text: string;
+};
+
 const Reviews = (props: Props) => {
-  return <div>Reviews</div>;
+  const arrReviews: Review[] = [
+    {
+      name: "Alex",
+      text: "bla bla",
+    },
+    {
+      name: "nik",
+      text: "bla bladfklfdlk",
+    },
+  ];
+
+  const [reviews, setReviews] = useState<Review[]>(arrReviews);
+
+  return (
+    <>
+      <Typography variant="h4" sx={{ margin: "50px 0" }}>
+        Reviews
+      </Typography>
+      <div>
+        {reviews.map(({ name, text }: Review, i) => (
+          <Card variant="outlined">
+            <CardContent>
+              <div key={i}>
+                name:{name}
+                text:{text}
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <form>
+        <h3>please leave a review</h3>
+        <div>
+          <TextField size="small" placeholder="your name" />
+        </div>
+        <div>
+          <TextareaAutosize minRows={5} placeholder="your text" />
+        </div>
+        <Button variant="outlined">Send</Button>
+      </form>
+    </>
+  );
 };
 export default Reviews;
